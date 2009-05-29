2cb769f72668a3029b7c2fb3bc9008f7c1c6b92b
Jens Reimann <jens.reimann@th4-systems.com>
2009-05-29 09:28:34 +0000

    fix mess
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@4168 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.aurora.utils/src/main/java/org/openscada/utils/concurrent/FutureListener.java
+++ org.openscada.aurora.utils/src/main/java/org/openscada/utils/concurrent/FutureListener.java
@@ -1,6 +1,6 @@
 /*
- * This file is part of the OpenSCADA import java.util.concurrent.Future;
-navare GmbH (http://inavare.com)
+ * This file is part of the OpenSCADA project
+ * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
