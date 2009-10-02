ec52d62752f83e4a9331034761bb4fc7559d39ee
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-02 09:35:23 +0000

    also sleep when deleting
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@5179 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ca.testing/src/org/openscada/ca/testing/ConfigurationFactoryImpl.java
+++ org.openscada.ca.testing/src/org/openscada/ca/testing/ConfigurationFactoryImpl.java
@@ -1,5 +1,6 @@
 package org.openscada.ca.testing;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.openscada.ca.ConfigurationFactory;
@@ -9,6 +10,8 @@ import org.slf4j.LoggerFactory;
 public class ConfigurationFactoryImpl implements ConfigurationFactory
 {
     private final static Logger logger = LoggerFactory.getLogger ( ConfigurationFactoryImpl.class );
+
+    private final Map<String, Map<String, String>> configurations = new HashMap<String, Map<String, String>> ();
 
     public void update ( final String configurationId, final Map<String, String> properties ) throws NumberFormatException, InterruptedException
     {
@@ -22,11 +25,22 @@ public class ConfigurationFactoryImpl implements ConfigurationFactory
         {
             Thread.sleep ( Integer.parseInt ( properties.get ( "sleep" ) ) );
         }
+        this.configurations.put ( configurationId, properties );
     }
 
-    public void delete ( final String configurationId )
+    public void delete ( final String configurationId ) throws NumberFormatException, InterruptedException
     {
         logger.info ( "Deleting: " + configurationId );
+
+        final Map<String, String> properties = this.configurations.remove ( configurationId );
+        if ( properties != null )
+        {
+            final String sleepStr = properties.get ( "sleep" );
+            if ( sleepStr != null )
+            {
+                Thread.sleep ( Integer.parseInt ( sleepStr ) );
+            }
+        }
     }
 
 }
