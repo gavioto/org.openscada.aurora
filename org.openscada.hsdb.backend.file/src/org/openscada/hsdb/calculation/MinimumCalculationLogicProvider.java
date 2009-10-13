4d7c247fb4ec4dd19c41dd76b43c9bfe45a9217c
Ludwig Sickinger <ludwig.sickinger@inavare.net>
2009-10-13 12:34:56 +0000

    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@5370 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.hsdb.backend.file/src/org/openscada/hsdb/calculation/MinimumCalculationLogicProvider.java
@@ -0,0 +1,117 @@
+package org.openscada.hsdb.calculation;
+
+import org.openscada.hsdb.datatypes.DataType;
+import org.openscada.hsdb.datatypes.DoubleValue;
+import org.openscada.hsdb.datatypes.LongValue;
+
+/**
+ * This class implements the CalculationLogicProvider interface for the calculation of minimum values.
+ * @author Ludwig Straub
+ */
+public class MinimumCalculationLogicProvider extends CalculationLogicProviderBase
+{
+    /**
+     * Constructor.
+     * @param inputDataType data type of the input values
+     * @param outputDataType data type of the output values
+     * @param parameters parameters further specifying the behaviour
+     */
+    public MinimumCalculationLogicProvider ( final DataType inputDataType, final DataType outputDataType, final long[] parameters )
+    {
+        super ( inputDataType, outputDataType, parameters );
+    }
+
+    /**
+     * @see org.openscada.hsdb.calculation.CalculationLogicProvider#getPassThroughValues
+     */
+    public boolean getPassThroughValues ()
+    {
+        return true;
+    }
+
+    /**
+     * @see org.openscada.hsdb.calculation.CalculationLogicProviderBase#generateLongValues
+     */
+    protected LongValue[] generateLongValues ( final LongValue[] values )
+    {
+        long minValue = Long.MAX_VALUE;
+        double quality = 0;
+        long baseValueCount = 0;
+        for ( LongValue value : values )
+        {
+            long val = value.getValue ();
+            if ( val < minValue )
+            {
+                minValue = val;
+            }
+            quality += value.getQualityIndicator ();
+            baseValueCount += value.getBaseValueCount ();
+        }
+        return new LongValue[] { new LongValue ( values[0].getTime (), quality / values.length, baseValueCount, minValue ) };
+    }
+
+    /**
+     * @see org.openscada.hsdb.calculation.CalculationLogicProviderBase#generateDoubleValues
+     */
+    protected LongValue[] generateLongValues ( final DoubleValue[] values )
+    {
+        double minValue = Double.MAX_VALUE;
+        double quality = 0;
+        long baseValueCount = 0;
+        for ( DoubleValue value : values )
+        {
+            double val = value.getValue ();
+            if ( val < minValue )
+            {
+                minValue = val;
+            }
+            quality += value.getQualityIndicator ();
+            baseValueCount += value.getBaseValueCount ();
+        }
+        return new LongValue[] { new LongValue ( values[0].getTime (), quality / values.length, baseValueCount, (long)minValue ) };
+    }
+
+    /**
+     * @see org.openscada.hsdb.calculation.CalculationLogicProviderBase#generateDoubleValues
+     */
+    protected DoubleValue[] generateDoubleValues ( final LongValue[] values )
+    {
+        long minValue = Long.MAX_VALUE;
+        double quality = 0;
+        long baseValueCount = 0;
+        for ( LongValue value : values )
+        {
+            long val = value.getValue ();
+            if ( val < minValue )
+            {
+                minValue = val;
+            }
+            quality += value.getQualityIndicator ();
+            baseValueCount += value.getBaseValueCount ();
+        }
+        return new DoubleValue[] { new DoubleValue ( values[0].getTime (), quality / values.length, baseValueCount, minValue ) };
+    }
+
+    /**
+     * This method generates double values for the time span starting with the first element in the array and ending after {@link #getRequiredTimespanForCalculation()}.
+     * @param values double values that were processed during the time span
+     * @return calculated double values
+     */
+    protected DoubleValue[] generateDoubleValues ( final DoubleValue[] values )
+    {
+        double minValue = Double.MAX_VALUE;
+        double quality = 0;
+        long baseValueCount = 0;
+        for ( DoubleValue value : values )
+        {
+            double val = value.getValue ();
+            if ( val < minValue )
+            {
+                minValue = val;
+            }
+            quality += value.getQualityIndicator ();
+            baseValueCount += value.getBaseValueCount ();
+        }
+        return new DoubleValue[] { new DoubleValue ( values[0].getTime (), quality / values.length, baseValueCount, minValue ) };
+    }
+}
