4db56a9ae7f2d99190a59a6725a3eed6b7634039
Ludwig Sickinger <ludwig.sickinger@inavare.net>
2009-10-26 13:24:05 +0000

    fixed daylight saving issue
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@5819 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hsdb.backend.file/src/org/openscada/hsdb/backend/file/FileBackEndFactory.java
+++ org.openscada.hsdb.backend.file/src/org/openscada/hsdb/backend/file/FileBackEndFactory.java
@@ -95,7 +95,7 @@ public class FileBackEndFactory implements BackEndFactory
     {
         final Calendar calendar = Calendar.getInstance ();
         calendar.setTimeInMillis ( time );
-        return String.format ( TIME_FORMAT, calendar.get ( Calendar.YEAR ), calendar.get ( Calendar.MONTH ), calendar.get ( Calendar.DAY_OF_MONTH ), calendar.get ( Calendar.HOUR_OF_DAY ), calendar.get ( Calendar.MINUTE ), calendar.get ( Calendar.SECOND ), calendar.get ( Calendar.MILLISECOND ), time );
+        return String.format ( TIME_FORMAT, calendar.get ( Calendar.YEAR ), calendar.get ( Calendar.MONTH ), calendar.get ( Calendar.DAY_OF_MONTH ), calendar.get ( Calendar.HOUR_OF_DAY ), calendar.get ( Calendar.MINUTE ), calendar.get ( Calendar.SECOND ), calendar.get ( Calendar.MILLISECOND ), calendar.get ( Calendar.DST_OFFSET ) );
     }
 
     /**
