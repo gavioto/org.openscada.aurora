5c26c0ec36376cae4fc1d0155f82b1e009478400
Jens Reimann <jens.reimann@th4-systems.com>
2009-10-27 11:14:46 +0000

    add more filter stuff
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@5863 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.utils.osgi/src/org/openscada/utils/osgi/FilterUtil.java
+++ org.openscada.utils.osgi/src/org/openscada/utils/osgi/FilterUtil.java
@@ -1,6 +1,7 @@
 package org.openscada.utils.osgi;
 
 import java.util.Map;
+import java.util.Set;
 
 import org.osgi.framework.Constants;
 import org.osgi.framework.Filter;
@@ -9,16 +10,79 @@ import org.osgi.framework.InvalidSyntaxException;
 
 public class FilterUtil
 {
-    public static Filter createAndFilter ( final String clazz, final Map<String, String> parameters ) throws InvalidSyntaxException
+
+    protected static Filter createFilter ( final String operand, final Map<String, String> parameters ) throws InvalidSyntaxException
     {
         final StringBuilder sb = new StringBuilder ();
 
-        sb.append ( "(&" );
+        sb.append ( "(" );
+        sb.append ( operand );
 
-        addPair ( sb, Constants.OBJECTCLASS, clazz );
         for ( final Map.Entry<String, String> entry : parameters.entrySet () )
         {
             addPair ( sb, entry.getKey (), entry.getValue () );
+        }
+        sb.append ( ")" );
+
+        return FrameworkUtil.createFilter ( sb.toString () );
+    }
+
+    public static Filter createClassFilter ( final String clazz ) throws InvalidSyntaxException
+    {
+        return FrameworkUtil.createFilter ( String.format ( "(%s=%s)", Constants.OBJECTCLASS, clazz ) );
+    }
+
+    public static Filter createAndFilter ( final Filter... filters ) throws InvalidSyntaxException
+    {
+        return createFilter ( "&", filters );
+    }
+
+    public static Filter createOrFilter ( final Filter... filters ) throws InvalidSyntaxException
+    {
+        return createFilter ( "|", filters );
+    }
+
+    protected static Filter createFilter ( final String operand, final Filter... filters ) throws InvalidSyntaxException
+    {
+        final StringBuilder sb = new StringBuilder ();
+
+        sb.append ( "(" );
+        sb.append ( operand );
+
+        for ( final Filter filter : filters )
+        {
+            sb.append ( filter.toString () );
+        }
+
+        sb.append ( ")" );
+
+        return FrameworkUtil.createFilter ( sb.toString () );
+    }
+
+    public static Filter createAndFilter ( final Map<String, String> parameters ) throws InvalidSyntaxException
+    {
+        return createFilter ( "&", parameters );
+    }
+
+    public static Filter createOrFilter ( final Map<String, String> parameters ) throws InvalidSyntaxException
+    {
+        return createFilter ( "|", parameters );
+    }
+
+    public static Filter createAndFilter ( final String clazz, final Map<String, String> parameters ) throws InvalidSyntaxException
+    {
+        return createAndFilter ( createClassFilter ( clazz ), createAndFilter ( parameters ) );
+    }
+
+    public static Filter createSimpleOr ( final String attribute, final Set<String> values ) throws InvalidSyntaxException
+    {
+        final StringBuilder sb = new StringBuilder ();
+
+        sb.append ( "(|" );
+
+        for ( final String value : values )
+        {
+            addPair ( sb, attribute, value );
         }
 
         sb.append ( ")" );
