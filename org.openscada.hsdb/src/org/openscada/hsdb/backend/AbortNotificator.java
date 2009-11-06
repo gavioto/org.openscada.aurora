31a6f029c3b594f803ab2ef9caec2722bad23dfd
Ludwig Sickinger <ludwig.sickinger@inavare.net>
2009-11-06 06:36:22 +0000

    documented interface
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@5979 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hsdb/src/org/openscada/hsdb/backend/AbortNotificator.java
+++ org.openscada.hsdb/src/org/openscada/hsdb/backend/AbortNotificator.java
@@ -1,6 +1,14 @@
 package org.openscada.hsdb.backend;
 
+/**
+ * This interface provides a method that can be used to check whether the processing of a method should be aborted or not.
+ * @author Ludwig Straub
+ */
 public interface AbortNotificator
 {
+    /**
+     * This method returns whether the processing of a method should be aborted or not
+     * @return true, if the processing should be aborted, otherwise false
+     */
     public abstract boolean getAbort ();
 }
