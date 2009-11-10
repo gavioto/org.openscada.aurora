a4515be6f94d002cd3d65e2b7c4b1356c841f950
Ludwig Sickinger <ludwig.sickinger@inavare.net>
2009-11-10 12:11:33 +0000

    reproduced and fixed misbehaviour on server
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6022 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hsdb.testing/src/org/openscada/hsdb/testing/TestSuite.java
+++ org.openscada.hsdb.testing/src/org/openscada/hsdb/testing/TestSuite.java
@@ -2,14 +2,14 @@ package org.openscada.hsdb.testing;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.openscada.hsdb.testing.compression.CalculatingStorageChannelTest;
+import org.openscada.hsdb.testing.backend.StorageBackEndTestSuite;
 
 /**
  * This class can be used as test entry point to perform all available tests for service classes.
  * @author Ludwig Straub
  */
 @RunWith ( Suite.class )
-@Suite.SuiteClasses ( { CalculatingStorageChannelTest.class } )
+@Suite.SuiteClasses ( { StorageBackEndTestSuite.class } )
 public class TestSuite
 {
 }
