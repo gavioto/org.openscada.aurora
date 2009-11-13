ccf3937a0591ee3d7d71e5b8f2dd7a1e9f4a7f48
Ludwig Sickinger <ludwig.sickinger@inavare.net>
2009-11-13 16:42:32 +0000

    changed test case: using data of juergen as input
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6103 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.hsdb.testing/src/org/openscada/hsdb/testing/TestSuite.java
+++ org.openscada.hsdb.testing/src/org/openscada/hsdb/testing/TestSuite.java
@@ -2,14 +2,14 @@ package org.openscada.hsdb.testing;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.openscada.hsdb.testing.backend.StorageBackEndTestSuite;
+import org.openscada.hsdb.testing.compression.CalculatingStorageChannelTest;
 
 /**
  * This class can be used as test entry point to perform all available tests for service classes.
  * @author Ludwig Straub
  */
 @RunWith ( Suite.class )
-@Suite.SuiteClasses ( { StorageBackEndTestSuite.class } )
+@Suite.SuiteClasses ( { CalculatingStorageChannelTest.class } )
 public class TestSuite
 {
 }
