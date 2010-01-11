cae684702c2ac8a032196811adc668eccadb573e
Jens Reimann <jens.reimann@th4-systems.com>
2010-01-11 16:35:56 +0000

    format
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6556 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.utils.toggle/src/org/openscada/utils/toggle/internal/ToggleInfo.java
+++ org.openscada.utils.toggle/src/org/openscada/utils/toggle/internal/ToggleInfo.java
@@ -2,46 +2,59 @@ package org.openscada.utils.toggle.internal;
 
 import java.io.Serializable;
 
-public class ToggleInfo implements Serializable {
-	
+public class ToggleInfo implements Serializable
+{
     private static final long serialVersionUID = -5949481833360048424L;
 
     private final int interval;
-	
-	private boolean on = false;
-	
-	public ToggleInfo(int interval) {
-		this.interval = interval;
-	}
-	
-	public int getInterval() {
-		return interval;
-	}
 
-	public boolean toggle() {
-		this.on = !this.on;
-		return !on;
-	}
+    private boolean on = false;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + interval;
-		return result;
-	}
+    public ToggleInfo ( final int interval )
+    {
+        this.interval = interval;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ToggleInfo other = (ToggleInfo) obj;
-		if (interval != other.interval)
-			return false;
-		return true;
-	}
+    public int getInterval ()
+    {
+        return this.interval;
+    }
+
+    public boolean toggle ()
+    {
+        this.on = !this.on;
+        return !this.on;
+    }
+
+    @Override
+    public int hashCode ()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.interval;
+        return result;
+    }
+
+    @Override
+    public boolean equals ( final Object obj )
+    {
+        if ( this == obj )
+        {
+            return true;
+        }
+        if ( obj == null )
+        {
+            return false;
+        }
+        if ( getClass () != obj.getClass () )
+        {
+            return false;
+        }
+        final ToggleInfo other = (ToggleInfo)obj;
+        if ( this.interval != other.interval )
+        {
+            return false;
+        }
+        return true;
+    }
 }
