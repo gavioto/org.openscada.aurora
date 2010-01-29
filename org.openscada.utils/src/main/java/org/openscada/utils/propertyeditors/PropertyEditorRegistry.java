7fc02c49b04b4ad8dd245512fdd6a756d4ffc76c
Jens Reimann <jens.reimann@th4-systems.com>
2010-01-29 17:22:36 +0000

    add default setup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6640 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.utils/src/main/java/org/openscada/utils/propertyeditors/PropertyEditorRegistry.java
+++ org.openscada.utils/src/main/java/org/openscada/utils/propertyeditors/PropertyEditorRegistry.java
@@ -3,7 +3,9 @@ package org.openscada.utils.propertyeditors;
 import java.beans.PropertyEditor;
 import java.beans.PropertyEditorManager;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -14,11 +16,42 @@ import java.util.concurrent.ConcurrentMap;
 public class PropertyEditorRegistry
 {
 
-    private ConcurrentMap<String, PropertyEditor> propertyEditors = new ConcurrentHashMap<String, PropertyEditor> ();
+    private final ConcurrentMap<String, PropertyEditor> propertyEditors = new ConcurrentHashMap<String, PropertyEditor> ();
+
+    public PropertyEditorRegistry ()
+    {
+        this ( false );
+    }
+
+    public PropertyEditorRegistry ( final boolean fillWithDefault )
+    {
+        if ( fillWithDefault )
+        {
+            registerCustomEditor ( Boolean.class, new BooleanEditor () );
+            registerCustomEditor ( boolean.class, new BooleanEditor () );
+            registerCustomEditor ( Byte.class, new ByteEditor () );
+            registerCustomEditor ( byte.class, new ByteEditor () );
+            registerCustomEditor ( Double.class, new DoubleEditor () );
+            registerCustomEditor ( double.class, new DoubleEditor () );
+            registerCustomEditor ( Float.class, new FloatEditor () );
+            registerCustomEditor ( float.class, new FloatEditor () );
+            registerCustomEditor ( Integer.class, new IntegerEditor () );
+            registerCustomEditor ( int.class, new IntegerEditor () );
+            registerCustomEditor ( Long.class, new LongEditor () );
+            registerCustomEditor ( long.class, new LongEditor () );
+            registerCustomEditor ( Short.class, new ShortEditor () );
+            registerCustomEditor ( short.class, new ShortEditor () );
+
+            registerCustomEditor ( String.class, new StringEditor () );
+            registerCustomEditor ( Date.class, new DateEditor () );
+            registerCustomEditor ( UUID.class, new UUIDEditor () );
+            registerCustomEditor ( Number.class, new NumberEditor () );
+        }
+    }
 
     public Map<String, PropertyEditor> getPropertyEditors ()
     {
-        return Collections.unmodifiableMap ( propertyEditors );
+        return Collections.unmodifiableMap ( this.propertyEditors );
     }
 
     /**
@@ -27,16 +60,16 @@ public class PropertyEditorRegistry
      * @return
      */
     @SuppressWarnings ( "unchecked" )
-    public PropertyEditor findCustomEditor ( Class requiredType, String propertyPath )
+    public PropertyEditor findCustomEditor ( final Class requiredType, final String propertyPath )
     {
         // first try to find exact match
         String key = requiredType.getCanonicalName () + ":" + propertyPath;
-        PropertyEditor pe = propertyEditors.get ( key );
+        PropertyEditor pe = this.propertyEditors.get ( key );
         // 2nd: try to find for class only
         if ( pe == null )
         {
             key = requiredType.getCanonicalName () + ":";
-            pe = propertyEditors.get ( key );
+            pe = this.propertyEditors.get ( key );
         }
         // 3rd: try to get internal
         if ( pe == null )
@@ -51,7 +84,7 @@ public class PropertyEditorRegistry
      * @return
      */
     @SuppressWarnings ( "unchecked" )
-    public PropertyEditor findCustomEditor ( Class requiredType )
+    public PropertyEditor findCustomEditor ( final Class requiredType )
     {
         return findCustomEditor ( requiredType, "" );
     }
@@ -61,7 +94,7 @@ public class PropertyEditorRegistry
      * @param propertyEditor
      */
     @SuppressWarnings ( "unchecked" )
-    public void registerCustomEditor ( Class requiredType, PropertyEditor propertyEditor )
+    public void registerCustomEditor ( final Class requiredType, final PropertyEditor propertyEditor )
     {
         registerCustomEditor ( requiredType, "", propertyEditor );
     }
@@ -72,9 +105,9 @@ public class PropertyEditorRegistry
      * @param propertyEditor
      */
     @SuppressWarnings ( "unchecked" )
-    public void registerCustomEditor ( Class requiredType, String propertyPath, PropertyEditor propertyEditor )
+    public void registerCustomEditor ( final Class requiredType, final String propertyPath, final PropertyEditor propertyEditor )
     {
-        String key = requiredType.getCanonicalName () + ":" + propertyPath;
-        propertyEditors.put ( key, propertyEditor );
+        final String key = requiredType.getCanonicalName () + ":" + propertyPath;
+        this.propertyEditors.put ( key, propertyEditor );
     }
 }
