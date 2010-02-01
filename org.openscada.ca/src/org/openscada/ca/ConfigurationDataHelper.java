45dc97f18cd3bf037a768bd686ba14a971d10977
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-01 15:45:40 +0000

    add methods for doubles
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6674 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ca/src/org/openscada/ca/ConfigurationDataHelper.java
+++ org.openscada.ca/src/org/openscada/ca/ConfigurationDataHelper.java
@@ -51,6 +51,29 @@ public class ConfigurationDataHelper
         return result;
     }
 
+    /**
+     * Get an integer from the configuration data
+     * <p>
+     * If the parameter is not set a {@link IllegalArgumentException} will be thrown. If
+     * the data can not be parsed to an integer a {@link NumberFormatException} will
+     * be thrown.
+     * </p>
+     * @param name the name of the parameter to get
+     * @param errorMessage the error message if the string is not set
+     * @return the value as integer
+     * @throws NumberFormatException
+     * @throws IllegalArgumentException
+     */
+    public int getIntegerChecked ( final String name, final String errorMessage ) throws IllegalArgumentException, NumberFormatException
+    {
+        final String str = this.data.get ( name );
+        if ( str == null )
+        {
+            throw new IllegalArgumentException ( errorMessage );
+        }
+        return Integer.parseInt ( str );
+    }
+
     public String getString ( final String name )
     {
         return this.data.get ( name );
@@ -90,6 +113,46 @@ public class ConfigurationDataHelper
     }
 
     /**
+     * Get an integer from the data or <code>null</code> if
+     * the parameter is not set or not an integer
+     * @param name the name of the parameter
+     * @return the integer or <code>null</code>
+     */
+    public Double getDouble ( final String name )
+    {
+        final String str = this.data.get ( name );
+        if ( str == null )
+        {
+            return null;
+        }
+        try
+        {
+            return Double.parseDouble ( str );
+        }
+        catch ( final NumberFormatException e )
+        {
+            return null;
+        }
+    }
+
+    /**
+     * Get an integer from the data or the default value if
+     * the parameter is not set or not an integer
+     * @param name the name of the parameter
+     * @param defaultValue the default value
+     * @return the integer or the default value
+     */
+    public double getDouble ( final String name, final double defaultValue )
+    {
+        final Double result = getDouble ( name );
+        if ( result == null )
+        {
+            return defaultValue;
+        }
+        return result;
+    }
+
+    /**
      * Get an integer from the configuration data
      * <p>
      * If the parameter is not set a {@link IllegalArgumentException} will be thrown. If
@@ -102,13 +165,14 @@ public class ConfigurationDataHelper
      * @throws NumberFormatException
      * @throws IllegalArgumentException
      */
-    public int getIntegerChecked ( final String name, final String errorMessage ) throws IllegalArgumentException, NumberFormatException
+    public double getDoubleChecked ( final String name, final String errorMessage ) throws IllegalArgumentException, NumberFormatException
     {
         final String str = this.data.get ( name );
         if ( str == null )
         {
             throw new IllegalArgumentException ( errorMessage );
         }
-        return Integer.parseInt ( str );
+        return Double.parseDouble ( str );
     }
+
 }
