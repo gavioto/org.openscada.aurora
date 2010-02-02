8c72256c72ea933344b589a6ba1ad394ed29351a
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-02 14:55:05 +0000

    add doc
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6686 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.utils.osgi/src/org/openscada/utils/osgi/pool/ObjectPoolHelper.java
+++ org.openscada.utils.osgi/src/org/openscada/utils/osgi/pool/ObjectPoolHelper.java
@@ -8,6 +8,17 @@ import org.osgi.framework.ServiceRegistration;
 
 public class ObjectPoolHelper
 {
+    /**
+     * Register the object pool with the OSGi service bus
+     * <p>
+     * The caller must ensure that the service is unregistered using the
+     * service registration object returned.
+     * </p>
+     * @param context the context used for registering
+     * @param pool the pool to register
+     * @param poolClass the service class provided by this pool
+     * @return the service registration
+     */
     public static ServiceRegistration registerObjectPool ( final BundleContext context, final ObjectPool pool, final String poolClass )
     {
         final Dictionary<Object, Object> properties = new Hashtable<Object, Object> ();
