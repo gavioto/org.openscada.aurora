2cbff627d2ce616f486cde985663dd330ee0695e
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-04 10:30:33 +0000

    is immutable
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6784 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.utils/src/org/openscada/utils/statuscodes/StatusCode.java
+++ org.openscada.utils/src/org/openscada/utils/statuscodes/StatusCode.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,47 +19,50 @@
 
 package org.openscada.utils.statuscodes;
 
+import org.openscada.utils.lang.Immutable;
+
+@Immutable
 public class StatusCode
 {
-    private String moduleCode;
+    private final String moduleCode;
 
-    private String subModuleCode;
+    private final String subModuleCode;
 
-    private long code;
+    private final long code;
 
-    private SeverityLevel severity;
+    private final SeverityLevel severity;
 
-    public StatusCode ( String module, String subModule, long code, SeverityLevel severity )
+    public StatusCode ( final String module, final String subModule, final long code, final SeverityLevel severity )
     {
-        moduleCode = module;
-        subModuleCode = subModule;
+        this.moduleCode = module;
+        this.subModuleCode = subModule;
         this.code = code;
         this.severity = severity;
     }
 
     public String getModuleCode ()
     {
-        return moduleCode;
+        return this.moduleCode;
     }
 
     public String getSubModuleCode ()
     {
-        return subModuleCode;
+        return this.subModuleCode;
     }
 
     public long getNumberCode ()
     {
-        return code;
+        return this.code;
     }
 
     public SeverityLevel getSeverity ()
     {
-        return severity;
+        return this.severity;
     }
 
     public String toString ()
     {
-        String statusCode = String.format ( "%s-%s-%08X", moduleCode, subModuleCode, code );
+        final String statusCode = String.format ( "%s-%s-%08X", this.moduleCode, this.subModuleCode, this.code );
         return statusCode;
     }
 
@@ -68,38 +71,54 @@ public class StatusCode
     {
         final int prime = 31;
         int result = 1;
-        result = prime * result + (int) ( code ^ ( code >>> 32 ) );
-        result = prime * result + ( ( moduleCode == null ) ? 0 : moduleCode.hashCode () );
-        result = prime * result + ( ( subModuleCode == null ) ? 0 : subModuleCode.hashCode () );
+        result = prime * result + (int) ( this.code ^ this.code >>> 32 );
+        result = prime * result + ( this.moduleCode == null ? 0 : this.moduleCode.hashCode () );
+        result = prime * result + ( this.subModuleCode == null ? 0 : this.subModuleCode.hashCode () );
         return result;
     }
 
     @Override
-    public boolean equals ( Object obj )
+    public boolean equals ( final Object obj )
     {
         if ( this == obj )
+        {
             return true;
+        }
         if ( obj == null )
+        {
             return false;
+        }
         if ( getClass () != obj.getClass () )
+        {
             return false;
+        }
         final StatusCode other = (StatusCode)obj;
-        if ( code != other.code )
+        if ( this.code != other.code )
+        {
             return false;
-        if ( moduleCode == null )
+        }
+        if ( this.moduleCode == null )
         {
             if ( other.moduleCode != null )
+            {
                 return false;
+            }
         }
-        else if ( !moduleCode.equals ( other.moduleCode ) )
+        else if ( !this.moduleCode.equals ( other.moduleCode ) )
+        {
             return false;
-        if ( subModuleCode == null )
+        }
+        if ( this.subModuleCode == null )
         {
             if ( other.subModuleCode != null )
+            {
                 return false;
+            }
         }
-        else if ( !subModuleCode.equals ( other.subModuleCode ) )
+        else if ( !this.subModuleCode.equals ( other.subModuleCode ) )
+        {
             return false;
+        }
         return true;
     }
 
