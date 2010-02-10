ad626e809596496160eff99110bcff0d13e5d736
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-10 08:41:09 +0000

    rename package
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@6906 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.ca.servlet.jaxws/src/org/openscada/ca/servelt/jaxws/ConfigurationAdministratorService.java
+++ org.openscada.ca.servlet.jaxws/src/org/openscada/ca/servelt/jaxws/ConfigurationAdministratorService.java
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package org.openscada.ca.servelt.axis;
+package org.openscada.ca.servelt.jaxws;
 
 import java.util.Collection;
 import java.util.LinkedList;
