8c434de9fca2d39ebd930643b26126fb95df8176
Jens Reimann <jens.reimann@th4-systems.com>
2010-02-18 17:45:03 +0000

    small initial load
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@7056 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.utils/src/org/openscada/utils/collection/MapBuilder.java
+++ org.openscada.utils/src/org/openscada/utils/collection/MapBuilder.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2009 inavare GmbH (http://inavare.com)
+ * Copyright (C) 2006-2010 inavare GmbH (http://inavare.com)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -46,7 +46,7 @@ public class MapBuilder<Key, Value>
 
     public MapBuilder ()
     {
-        this.map = new HashMap<Key, Value> ();
+        this.map = new HashMap<Key, Value> ( 0 );
     }
 
     /**
