928491b924d2cf243e8c542942a7479d33c4439f
Jens Reimann <jens.reimann@th4-systems.com>
2010-03-14 15:33:42 +0000

    cleanup
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@7416 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.sec.provider.plain.property/src/org/openscada/sec/provider/plain/property/Activator.java
+++ org.openscada.sec.provider.plain.property/src/org/openscada/sec/provider/plain/property/Activator.java
@@ -17,13 +17,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package org.openscada.sec.provider.plain.property.internal;
+package org.openscada.sec.provider.plain.property;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 import org.openscada.sec.AuthenticationService;
-import org.openscada.sec.provider.plain.property.PropertyAuthenticationService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
