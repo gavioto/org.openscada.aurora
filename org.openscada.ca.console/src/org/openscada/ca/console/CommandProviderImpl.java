e6445b898608d22f51a5361adfa6fc22952f4b32
Jens Reimann <jens.reimann@th4-systems.com>
2010-03-18 19:01:52 +0000

    initial import
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@7462 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.ca.console/src/org/openscada/ca/console/CommandProviderImpl.java
@@ -0,0 +1,103 @@
+package org.openscada.ca.console;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.eclipse.osgi.framework.console.CommandInterpreter;
+import org.eclipse.osgi.framework.console.CommandProvider;
+import org.openscada.ca.FreezableConfigurationAdministrator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class CommandProviderImpl implements CommandProvider
+{
+
+    private ServiceRegistration reg;
+
+    private ServiceTracker caTracker;
+
+    public CommandProviderImpl ()
+    {
+    }
+
+    public void start ( final BundleContext context )
+    {
+        this.caTracker = new ServiceTracker ( context, FreezableConfigurationAdministrator.class.getName (), null );
+        this.caTracker.open ();
+
+        final Dictionary<String, Object> properties = new Hashtable<String, Object> ();
+        this.reg = context.registerService ( CommandProvider.class.getName (), this, properties );
+    }
+
+    public void stop ()
+    {
+        this.caTracker.close ();
+        this.caTracker = null;
+
+        this.reg.unregister ();
+        this.reg = null;
+    }
+
+    public String getHelp ()
+    {
+        final StringBuilder sb = new StringBuilder ();
+        sb.append ( "\n---Configuration Administrator Commands---\n" ); //$NON-NLS-1$
+        sb.append ( "\tfreezeCfgAdmin - stop announcing changes\n" ); //$NON-NLS-1$
+        sb.append ( "\tthawCfgAdmin - start announcing changes\n" ); //$NON-NLS-1$
+        return sb.toString ();
+    }
+
+    public void _freezeCfgAdmin ( final CommandInterpreter cmd )
+    {
+        final Object[] services = this.caTracker.getServices ();
+        final int count = services != null ? services.length : 0;
+        cmd.print ( String.format ( "Freezing %s configuration administrators", count ) );
+        if ( services != null )
+        {
+            for ( final Object o : services )
+            {
+                if ( o instanceof FreezableConfigurationAdministrator )
+                {
+                    cmd.println ( String.format ( "Freeze: {}", o ) );
+                    try
+                    {
+                        ( (FreezableConfigurationAdministrator)o ).freeze ();
+                    }
+                    catch ( final Exception e )
+                    {
+                        cmd.println ( "Failed to freeze" );
+                        cmd.printStackTrace ( e );
+                    }
+                }
+            }
+        }
+    }
+
+    public void _thawCfgAdmin ( final CommandInterpreter cmd )
+    {
+        final Object[] services = this.caTracker.getServices ();
+        final int count = services != null ? services.length : 0;
+        cmd.print ( String.format ( "Thawing %s configuration administrators", count ) );
+        if ( services != null )
+        {
+            for ( final Object o : services )
+            {
+                if ( o instanceof FreezableConfigurationAdministrator )
+                {
+                    cmd.println ( String.format ( "Thaw: {}", o ) );
+                    try
+                    {
+                        ( (FreezableConfigurationAdministrator)o ).thaw ();
+                    }
+                    catch ( final Exception e )
+                    {
+                        cmd.println ( "Failed to thaw" );
+                        cmd.printStackTrace ( e );
+                    }
+                }
+            }
+        }
+    }
+
+}
