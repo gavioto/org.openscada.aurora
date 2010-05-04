e8d80b87d6c0e76302b27e32eaa9f63a150e9311
Juergen Rose <juergen.rose@th4-systems.com>
2010-05-04 17:16:30 +0000

    alternative to xml interface (unfortunately still a mix of gson/jackson) because of bug http://code.google.com/p/google-gson/issues/detail?id=197
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@7816 1b50e8ee-deed-0310-b1de-ab401e1c4d63

---
+++ org.openscada.ca.servlet.json/src/org/openscada/ca/servlet/json/Activator.java
@@ -0,0 +1,173 @@
+package org.openscada.ca.servlet.json;
+
+import javax.servlet.ServletException;
+
+import org.openscada.ca.ConfigurationAdministrator;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
+
+public class Activator implements BundleActivator
+{
+
+    private BundleContext context;
+
+    private ServiceTracker configurationAdminTracker;
+
+    private ServiceTracker httpServiceTracker;
+
+    private ConfigurationAdministrator configurationAdmin;
+
+    private HttpService httpService;
+
+    private HttpContext httpContext;
+
+    private JsonServlet servlet;
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+     */
+    public void start ( BundleContext context ) throws Exception
+    {
+        this.context = context;
+
+        ServiceTrackerCustomizer configurationAdminCustomizer = createConfigurationAdminCustomizer ();
+        configurationAdminTracker = new ServiceTracker ( context, ConfigurationAdministrator.class.getName (), configurationAdminCustomizer );
+        ServiceTrackerCustomizer httpServiceCustomizer = createHttpServiceCustomizer ();
+        httpServiceTracker = new ServiceTracker ( context, HttpService.class.getName (), httpServiceCustomizer );
+
+        configurationAdminTracker.open ();
+        httpServiceTracker.open ();
+    }
+
+    private ServiceTrackerCustomizer createConfigurationAdminCustomizer ()
+    {
+        return new ServiceTrackerCustomizer () {
+            @Override
+            public Object addingService ( ServiceReference reference )
+            {
+                Object service = context.getService ( reference );
+                synchronized ( Activator.this )
+                {
+                    if ( Activator.this.configurationAdmin == null )
+                    {
+                        Activator.this.configurationAdmin = (ConfigurationAdministrator)service;
+                        Activator.this.bind ();
+                    }
+                }
+                return service;
+            }
+
+            @Override
+            public void modifiedService ( ServiceReference reference, Object service )
+            {
+                // pass
+            }
+
+            @Override
+            public void removedService ( ServiceReference reference, Object service )
+            {
+                synchronized ( Activator.this )
+                {
+                    if ( Activator.this.configurationAdmin != service )
+                    {
+                        return;
+                    }
+                    Activator.this.unbind ();
+                    Activator.this.configurationAdmin = null;
+                    Activator.this.bind ();
+                }
+            }
+        };
+    }
+
+    private ServiceTrackerCustomizer createHttpServiceCustomizer ()
+    {
+        return new ServiceTrackerCustomizer () {
+            @Override
+            public Object addingService ( ServiceReference reference )
+            {
+                Object service = context.getService ( reference );
+                synchronized ( Activator.this )
+                {
+                    if ( Activator.this.httpService == null )
+                    {
+                        Activator.this.httpService = (HttpService)service;
+                        Activator.this.bind ();
+                    }
+                }
+                return service;
+            }
+
+            @Override
+            public void modifiedService ( ServiceReference reference, Object service )
+            {
+                // pass
+            }
+
+            @Override
+            public void removedService ( ServiceReference reference, Object service )
+            {
+                synchronized ( Activator.this )
+                {
+                    if ( Activator.this.httpService != service )
+                    {
+                        return;
+                    }
+                    Activator.this.unbind ();
+                    Activator.this.httpService = null;
+                    Activator.this.bind ();
+                }
+            }
+        };
+    }
+
+    private void bind ()
+    {
+        if ( this.httpService != null && this.configurationAdmin != null )
+        {
+            this.httpContext = this.httpService.createDefaultHttpContext ();
+            try
+            {
+                this.httpService.registerServlet ( "/org.openscada.ca", this.servlet = new JsonServlet ( this.configurationAdmin ), null, this.httpContext );
+            }
+            catch ( final NamespaceException e )
+            {
+                e.printStackTrace ();
+            }
+            catch ( final ServletException e )
+            {
+                e.printStackTrace ();
+            }
+        }
+    }
+
+    private void unbind ()
+    {
+        if ( this.httpService != null )
+        {
+            this.httpService.unregister ( "/org.openscada.ca" );
+
+            this.servlet.destroy ();
+            this.servlet = null;
+
+            this.httpContext = null;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop ( BundleContext context ) throws Exception
+    {
+        httpServiceTracker.close ();
+        configurationAdminTracker.close ();
+    }
+}
---
+++ org.openscada.ca.servlet.json/src/org/openscada/ca/servlet/json/JsonServlet.java
@@ -0,0 +1,368 @@
+package org.openscada.ca.servlet.json;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.Future;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.openscada.ca.Configuration;
+import org.openscada.ca.ConfigurationAdministrator;
+import org.openscada.ca.DiffEntry;
+import org.openscada.ca.Factory;
+import org.openscada.ca.DiffEntry.Operation;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import com.google.gson.reflect.TypeToken;
+
+public class JsonServlet extends HttpServlet
+{
+    private class FactorySerializer implements JsonSerializer<Factory>
+    {
+        @Override
+        public JsonElement serialize ( Factory factory, Type typeOfFactory, JsonSerializationContext context )
+        {
+            JsonObject obj = new JsonObject ();
+            obj.addProperty ( "id", factory.getId () );
+            obj.addProperty ( "description", factory.getDescription () );
+            obj.addProperty ( "state", factory.getState ().toString () );
+            return obj;
+        }
+    }
+
+    private class DiffEntrySerializer implements JsonSerializer<DiffEntry>, JsonDeserializer<DiffEntry>
+    {
+        @Override
+        public JsonElement serialize ( DiffEntry config, Type typeOfDiffEntry, JsonSerializationContext context )
+        {
+            JsonObject obj = new JsonObject ();
+            obj.addProperty ( "factoryId", config.getFactoryId () );
+            obj.addProperty ( "configurationId", config.getConfigurationId () );
+            obj.addProperty ( "operation", config.getOperation ().toString () );
+            JsonObject map = new JsonObject ();
+            for ( Entry<?, ?> entry : config.getData ().entrySet () )
+            {
+                map.addProperty ( String.valueOf ( entry.getKey () ), String.valueOf ( entry.getValue () ) );
+            }
+            obj.add ( "data", map );
+            return obj;
+        }
+
+        @Override
+        public DiffEntry deserialize ( JsonElement element, Type type, JsonDeserializationContext context ) throws JsonParseException
+        {
+            final String factoryId = element.getAsJsonObject ().get ( "factoryId" ).getAsString ();
+            final String configurationId = element.getAsJsonObject ().get ( "configurationId" ).getAsString ();
+            final String operation = element.getAsJsonObject ().get ( "operation" ).getAsString ();
+            final Map<String, String> data = context.deserialize ( element.getAsJsonObject ().getAsJsonObject ( "data" ), new TypeToken<Map<String, String>> () {}.getType () );
+            return new DiffEntry ( factoryId, configurationId, Operation.valueOf ( operation ), data );
+        }
+    }
+
+    private static final long serialVersionUID = -3311156226543946433L;
+
+    private final ConfigurationAdministrator configurationAdmin;
+
+    private final Gson gson = new GsonBuilder ().setPrettyPrinting ().registerTypeAdapter ( Factory.class, new FactorySerializer () ).registerTypeAdapter ( DiffEntry.class, new DiffEntrySerializer () ).create ();
+
+    private final ObjectMapper mapper = new ObjectMapper ();
+
+    public JsonServlet ( ConfigurationAdministrator configurationAdmin )
+    {
+        this.configurationAdmin = configurationAdmin;
+    }
+
+    @Override
+    protected void doGet ( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException
+    {
+        if ( req.getPathInfo () == null || "/".equals ( req.getPathInfo () ) )
+        {
+            redirectToFactoryList ( req, resp );
+            return;
+        }
+        else if ( req.getPathInfo ().startsWith ( "/factory" ) )
+        {
+            getFactory ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/knownFactories" ) )
+        {
+            getKnownFactories ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/configurations" ) )
+        {
+            getConfigurations ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/configuration" ) )
+        {
+            getConfiguration ( req, resp );
+        }
+        else
+        {
+            send404Error ( req, resp );
+        }
+    }
+
+    @Override
+    protected void doPost ( HttpServletRequest req, HttpServletResponse resp ) throws ServletException, IOException
+    {
+        if ( req.getPathInfo ().startsWith ( "/createConfiguration" ) )
+        {
+            createConfiguration ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/updateConfiguration" ) )
+        {
+            updateConfiguration ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/deleteConfiguration" ) )
+        {
+            deleteConfiguration ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/purgeFactory" ) )
+        {
+            purgeFactory ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/createDiff" ) )
+        {
+            createDiff ( req, resp );
+        }
+        else if ( req.getPathInfo ().startsWith ( "/applyDiff" ) )
+        {
+            applyDiff ( req, resp );
+        }
+        else
+        {
+            send404Error ( req, resp );
+        }
+    }
+
+    private void defaultContentType ( HttpServletResponse resp )
+    {
+        resp.setContentType ( "text/javascript" );
+    }
+
+    private void send404Error ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        resp.sendError ( HttpServletResponse.SC_NOT_FOUND, "Not Found" );
+    }
+
+    private void redirectToFactoryList ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        resp.setHeader ( "Location", req.getServletPath () + "/knownFactories" );
+        resp.sendError ( HttpServletResponse.SC_MOVED_PERMANENTLY, "Moved Permanently" );
+    }
+
+    private void getFactory ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        String factoryId = getRequiredString ( req, resp, "factory.id" );
+        gson.toJson ( configurationAdmin.getFactory ( factoryId ), Factory.class, resp.getWriter () );
+    }
+
+    private void getKnownFactories ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        gson.toJson ( configurationAdmin.getKnownFactories (), Factory[].class, resp.getWriter () );
+    }
+
+    private void getConfigurations ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        String factoryId = getRequiredString ( req, resp, "factory.id" );
+        gson.toJson ( configurationAdmin.getConfigurations ( factoryId ), resp.getWriter () );
+    }
+
+    private void getConfiguration ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        String factoryId = getRequiredString ( req, resp, "factory.id" );
+        String id = getRequiredString ( req, resp, "id" );
+        gson.toJson ( configurationAdmin.getConfiguration ( factoryId, id ), resp.getWriter () );
+    }
+
+    private void createConfiguration ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        try
+        {
+            final String factoryId = getRequiredString ( req, resp, "factory.id" );
+            final String id = getRequiredString ( req, resp, "id" );
+            final Map<String, String> properties = gson.fromJson ( req.getReader (), new TypeToken<Map<String, String>> () {}.getType () );
+            final Future<Configuration> future = configurationAdmin.createConfiguration ( factoryId, id, properties );
+            gson.toJson ( future.get (), resp.getWriter () );
+        }
+        catch ( Exception e )
+        {
+            throw new ServletException ( e );
+        }
+    }
+
+    private void updateConfiguration ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        try
+        {
+            final String factoryId = getRequiredString ( req, resp, "factory.id" );
+            final String id = getRequiredString ( req, resp, "id" );
+            final String full = getOptionalString ( req, resp, "full", "" );
+            final Map<String, String> properties = gson.fromJson ( req.getReader (), new TypeToken<Map<String, String>> () {}.getType () );
+            final Future<Configuration> future = configurationAdmin.updateConfiguration ( factoryId, id, properties, "full".equals ( full ) );
+            gson.toJson ( future.get (), resp.getWriter () );
+        }
+        catch ( Exception e )
+        {
+            throw new ServletException ( e );
+        }
+    }
+
+    private void deleteConfiguration ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        try
+        {
+            final String factoryId = getRequiredString ( req, resp, "factory.id" );
+            final String id = getRequiredString ( req, resp, "id" );
+            final Future<Configuration> future = configurationAdmin.deleteConfiguration ( factoryId, id );
+            gson.toJson ( future.get (), resp.getWriter () );
+        }
+        catch ( Exception e )
+        {
+            throw new ServletException ( e );
+        }
+    }
+
+    private void purgeFactory ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        try
+        {
+            final String factoryId = getRequiredString ( req, resp, "factory.id" );
+            final Future<Void> future = configurationAdmin.purgeFactory ( factoryId );
+            gson.toJson ( future.get (), resp.getWriter () );
+        }
+        catch ( Exception e )
+        {
+            throw new ServletException ( e );
+        }
+    }
+
+    @SuppressWarnings ( "unchecked" )
+    private void createDiff ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+
+        Map newConfiguration = mapper.readValue ( req.getReader (), HashMap.class );
+
+        Map existingConfiguration = new HashMap ();
+        // convert existing configuration to same format as new one
+        for ( final Factory factory : configurationAdmin.getKnownFactories () )
+        {
+            Map config = new HashMap ();
+            existingConfiguration.put ( factory.getId (), config );
+            for ( Configuration configuration : configurationAdmin.getConfigurations ( factory.getId () ) )
+            {
+                config.put ( configuration.getId (), new HashMap ( configuration.getData () ) );
+            }
+        }
+
+        // create diff
+        List<DiffEntry> diff = new ArrayList<DiffEntry> ();
+
+        // 1. find added
+        for ( Entry<String, Map<String, Map<String, String>>> factory : ( (Map<String, Map<String, Map<String, String>>>)newConfiguration ).entrySet () )
+        {
+            String factoryId = factory.getKey ();
+            Map<String, Map<String, String>> existingConfigs = (Map<String, Map<String, String>>)existingConfiguration.get ( factoryId );
+            for ( final Entry<String, Map<String, String>> config : ( (Map<String, Map<String, String>>)newConfiguration.get ( factoryId ) ).entrySet () )
+            {
+                String id = config.getKey ();
+                Map<String, String> data = config.getValue ();
+                if ( existingConfigs == null )
+                {
+                    diff.add ( new DiffEntry ( factoryId, id, DiffEntry.Operation.ADD, new HashMap ( data ) ) );
+                }
+                else if ( !existingConfigs.containsKey ( id ) )
+                {
+                    diff.add ( new DiffEntry ( factoryId, id, DiffEntry.Operation.ADD, new HashMap ( data ) ) );
+                }
+                else if ( !data.equals ( existingConfigs.get ( id ) ) )
+                {
+                    diff.add ( new DiffEntry ( factoryId, id, DiffEntry.Operation.UPDATE, new HashMap ( data ) ) );
+                }
+            }
+        }
+        // find deleted
+        for ( Entry<String, Map<String, Map<String, String>>> factory : ( (Map<String, Map<String, Map<String, String>>>)existingConfiguration ).entrySet () )
+        {
+            String factoryId = factory.getKey ();
+            Map<String, Map<String, String>> newConfigs = (Map<String, Map<String, String>>)newConfiguration.get ( factoryId );
+            for ( Entry<String, Map<String, String>> config : ( (Map<String, Map<String, String>>)existingConfiguration.get ( factoryId ) ).entrySet () )
+            {
+                String id = config.getKey ();
+                if ( newConfigs == null )
+                {
+                    diff.add ( new DiffEntry ( factoryId, id, DiffEntry.Operation.DELETE, new HashMap () ) );
+                }
+                else if ( !newConfigs.containsKey ( id ) )
+                {
+                    diff.add ( new DiffEntry ( factoryId, id, DiffEntry.Operation.DELETE, new HashMap () ) );
+                }
+            }
+        }
+        gson.toJson ( diff, resp.getWriter () );
+    }
+
+    private void applyDiff ( HttpServletRequest req, HttpServletResponse resp ) throws IOException, ServletException
+    {
+        defaultContentType ( resp );
+        try
+        {
+            Collection<DiffEntry> changeSet = gson.fromJson ( req.getReader (), new TypeToken<Collection<DiffEntry>> () {}.getType () );
+            Future<Void> future = configurationAdmin.applyDiff ( changeSet );
+            future.get ();
+            resp.getWriter ().print ( changeSet.size () + " applied" );
+        }
+        catch ( Exception e )
+        {
+            throw new ServletException ( e );
+        }
+    }
+
+    private String getRequiredString ( HttpServletRequest req, HttpServletResponse resp, String key ) throws ServletException, IOException
+    {
+        Object o = req.getParameterMap ().get ( key );
+        if ( o == null )
+        {
+            resp.getWriter ().print ( "parameter " + key + " must not be null" );
+            throw new ServletException ( "parameter " + key + " must not be null" );
+        }
+        return ( (String[])o )[0];
+    }
+
+    private String getOptionalString ( HttpServletRequest req, HttpServletResponse resp, String key, String def )
+    {
+        Object o = req.getParameterMap ().get ( key );
+        if ( o == null )
+        {
+            return def;
+        }
+        return ( (String[])o )[0];
+    }
+}
