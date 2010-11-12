5b9fd265cf2795e49743dd947834d3c19302cfba
Jens Reimann <jens.reimann@th4-systems.com>
2010-11-12 15:22:29 +0000

    change to TH4 Systems
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/aurora/trunk@8478 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.utils/src/org/openscada/utils/lang/Holder.java
+++ org.openscada.utils/src/org/openscada/utils/lang/Holder.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://inavare.com)
+ * Copyright (C) 2006-2010 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -23,7 +23,7 @@ package org.openscada.utils.lang;
  * This class is inteded to by used for passing objects as "reference to
  * reference".
  * 
- * @author Jens Reimann <jens.reimann@inavare.net>
+ * @author Jens Reimann <jens.reimann@th4-systems.com>
  * @param <T>
  *            type that should be wrapped
  */
