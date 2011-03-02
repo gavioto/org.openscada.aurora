1096b6886f6437b310064be6967f397d72dfb5d4
Jens Reimann <jens.reimann@th4-systems.com>
2011-03-02 18:47:12 +0100

    format source

--- org.openscada.utils/src/org/openscada/utils/codec/Base64.java
+++ org.openscada.utils/src/org/openscada/utils/codec/Base64.java
@@ -1255,7 +1255,7 @@ public class Base64
         {
             bytes = s.getBytes ();
         } // end catch
-        //</change>
+          //</change>
 
         // Decode
         bytes = decode ( bytes, 0, bytes.length, options );
