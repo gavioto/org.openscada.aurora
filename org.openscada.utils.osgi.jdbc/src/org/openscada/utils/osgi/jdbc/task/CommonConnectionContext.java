2d34805a98118172ccdd977ad875965489640281
Jens Reimann <jens.reimann@th4-systems.com>
2012-01-25 09:56:41 +0100

    actually we don't need isClosed

--- org.openscada.utils.osgi.jdbc/src/org/openscada/utils/osgi/jdbc/task/CommonConnectionContext.java
+++ org.openscada.utils.osgi.jdbc/src/org/openscada/utils/osgi/jdbc/task/CommonConnectionContext.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -139,10 +139,7 @@ public abstract class CommonConnectionContext implements ConnectionContext
             {
                 if ( rs != null )
                 {
-                    if ( !rs.isClosed () )
-                    {
-                        rs.close ();
-                    }
+                    rs.close ();
                 }
             }
         }
