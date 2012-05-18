149946ca2f9bc5fb8b238f6428b21b9383b63bba
Jens Reimann <jens.reimann@th4-systems.com>
2012-05-18 13:39:55 +0200

    fix properties

--- org.openscada.sec.provider.jdbc/src/org/openscada/sec/provider/jdbc/Activator.java
+++ org.openscada.sec.provider.jdbc/src/org/openscada/sec/provider/jdbc/Activator.java
@@ -54,8 +54,8 @@ public class Activator implements BundleActivator
 
         this.serviceFactory = new JdbcAuthenticationServiceFactory ( bundleContext );
         final Dictionary<String, Object> properties = new Hashtable<String, Object> ();
-        properties.put ( Constants.BUNDLE_DESCRIPTION, "A JDBC authentication service" );
-        properties.put ( Constants.BUNDLE_VENDOR, "TH4 SYSTEMS GmbH" );
+        properties.put ( Constants.SERVICE_DESCRIPTION, "A JDBC authentication service" );
+        properties.put ( Constants.SERVICE_VENDOR, "TH4 SYSTEMS GmbH" );
         properties.put ( ConfigurationAdministrator.FACTORY_ID, bundleContext.getBundle ().getSymbolicName () + ".authenticator" );
         this.handle = bundleContext.registerService ( ConfigurationFactory.class, this.serviceFactory, properties );
     }
