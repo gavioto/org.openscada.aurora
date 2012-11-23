2a8d2cec33f0fd1834f0ecc4c7aa02e29140cef1
Jens Reimann <jens.reimann@th4-systems.com>
2012-11-23 11:41:30 +0100

    re-order ctors

--- org.openscada.utils/src/org/openscada/utils/concurrent/NamedThreadFactory.java
+++ org.openscada.utils/src/org/openscada/utils/concurrent/NamedThreadFactory.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the OpenSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * OpenSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -44,6 +44,11 @@ public class NamedThreadFactory implements ThreadFactory
         this ( name, false );
     }
 
+    public NamedThreadFactory ( final String name, final boolean daemon )
+    {
+        this ( name, daemon, true );
+    }
+
     public NamedThreadFactory ( final String name, final boolean daemon, final boolean logExceptions )
     {
         this.logExceptions = logExceptions;
@@ -54,11 +59,6 @@ public class NamedThreadFactory implements ThreadFactory
         {
             throw new IllegalArgumentException ( String.format ( "'name' must not be null" ) );
         }
-    }
-
-    public NamedThreadFactory ( final String name, final boolean daemon )
-    {
-        this ( name, daemon, true );
     }
 
     @Override
